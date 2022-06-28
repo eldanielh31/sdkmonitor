5f39e63932c32dae15b4b16d41569a8567a71fe9
Daniel <danbg31@gmail.com>
2022-06-28 17:00:51 -0600

    login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,43 +1,48 @@
 import React from 'react'
 import "./login.css"
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+    const navigate = useNavigate();
+
+    const handleLogin = ()=>{
+        navigate("/");
+    };
+
     return (
-        <div>
-            <div className='containerLogin'>
-                <div className='wrapperLogin'>
-                    <h1 className="titleLogin">LOGIN</h1>
-                    <form className="formLogin">
-                        <div className="groupLogin">
-                            <input type="text" className='inputLogin' required />
-                            <span className="highlightLogin"></span>
-                            <span className="barLogin"></span>
-                            <label className='labelLogin'>IP</label>
-                        </div>
+        <div className='containerLogin'>
+            <div className='wrapperLogin'>
+                <h1 className="titleLogin">LOGIN</h1>
+                <form className="formLogin">
+                    <div className="groupLogin">
+                        <input type="text" className='inputLogin' required />
+                        <span className="highlightLogin"></span>
+                        <span className="barLogin"></span>
+                        <label className='labelLogin'>IP</label>
+                    </div>
 
-                        <div className="groupLogin">
-                            <input type="password" className='inputLogin' required />
-                            <span className="highlightLogin"></span>
-                            <span className="barLogin"></span>
-                            <label className='labelLogin'>User</label>
-                        </div>
+                    <div className="groupLogin">
+                        <input type="password" className='inputLogin' required />
+                        <span className="highlightLogin"></span>
+                        <span className="barLogin"></span>
+                        <label className='labelLogin'>User</label>
+                    </div>
 
-                        <div className="groupLogin">
-                            <input type="password" className='inputLogin' required />
-                            <span className="highlightLogin"></span>
-                            <span className="barLogin"></span>
-                            <label className='labelLogin'>Password</label>
-                        </div>
+                    <div className="groupLogin">
+                        <input type="password" className='inputLogin' required />
+                        <span className="highlightLogin"></span>
+                        <span className="barLogin"></span>
+                        <label className='labelLogin'>Password</label>
+                    </div>
 
-                        <div className="groupLogin">
-                            <button className="button-57">
-                                <span className="text"> LOGIN </span>
-                                <span>OK.</span>
-                            </button>
-                        </div>
+                    <div className="groupLogin">
+                        <button className="button-57" onClick={handleLogin}>
+                            <span className="text"> LOGIN </span>
+                            <span>OK.</span>
+                        </button>
+                    </div>
 
-                    </form>
-                </div>
+                </form>
             </div>
         </div>
     )
