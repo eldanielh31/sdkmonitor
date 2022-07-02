3af731330e056c7cd41c544de3b6cb28b0524932
Daniel <danbg31@gmail.com>
2022-07-01 21:05:35 -0600

    grafica cpu v1

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -8,7 +8,6 @@ function Login() {
     const handleLogin = () => {
         setTextError("Error");
     };
-
     return (
         <div className='containerLogin'>
             <div className='wrapperLogin'>
