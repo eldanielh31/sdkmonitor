e4e04c5366377b4d3acb73c723bce8889af995aa
Ernesto <ernevarado@gmail.com>
2022-07-13 03:24:21 -0600

    Home Page V1.0

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar'
 import "./home.css"
+import imagehome from "../../components/recursos/Hewlett_Packard_Enterprise_logo.svg.png"
 
 function Home() {
   return (
@@ -8,13 +9,14 @@ function Home() {
       <Sidebar />
       <div className='containerHOME'>
         
-        <div><h1 className='Titulo1'>Bienvenido -nombre de usuario- al SDK Monitor</h1></div>
+        <div className ='Titulo1'>Bienvenido al SDK Monitor</div>
         
-        <div className='Titulo2'>Utilice la barra lateral para ingresar a las estadísticas actuales del switch</div>
-        
+        <div className='Titulo2'>Utilice la barra lateral para ingresar a las estadísticas actuales del switch</div>     
 
-        Home
       </div>
+
+      <img class="logohome" src={imagehome}  />
+      
     </div>
   )
 }
