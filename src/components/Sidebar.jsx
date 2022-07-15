e676a1c145d2d7f0fc1028f646fabb6156be086c
Daniel <danbg31@gmail.com>
2022-07-15 15:55:35 -0600

    revert logout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,31 +1,63 @@
 import React from 'react'
-import {SidebarData} from './SidebarData'
+import { SidebarData } from './SidebarData'
 import "./sidebar.css"
+import LogoutIcon from '@mui/icons-material/Logout';
 import image from "./recursos/hpe_pri_grn_rev_rgb-1.png"
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/userRedux';
 
 function Sidebar() {
-  return (
-    <div className="Sidebar">
-        <ul className="SidebarList">
-            {SidebarData.map((val, key)=> {
-                return (
-                    <li
-                        key={key}
-                        className="row"
-                        id={window.location.pathname === val.link ? "active" : ""} 
-                        onClick={()=>{
-                            window.location.pathname = val.link
-                        }}
-                    > 
-                        <div id="icon">{val.icon}</div> <div id="title" > {val.title} </div>
-                    </li>
-                );
-            })}
-        </ul>
-        <img src={image}  />
-        <button class="button-87" role="button">Log Out</button>
-    </div>
-  )
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        dispatch(deleteUserStart());
+        try {
+            dispatch(deleteUserSuccess())
+            navigate('/login')
+        } catch (error) {
+            dispatch(deleteUserFailure())
+            console.log(error);
+        }
+    };
+
+    return (
+        <div className="Sidebar">
+            <center>
+                <img className='imgSideBar' src={URL = 'https://img2.storyblok.com/450x233/smart/filters:quality(100)/f/70749/450x233/eba2433f96/aruba_hp_lockup_reversed.png'} alt="" width="82%" />
+            </center>
+            <ul className="SidebarList">
+                {SidebarData.map((val, key) => {
+                    return (
+                        <li
+                            key={key}
+                            className="row"
+                            id={window.location.pathname === val.link ? "active" : ""}
+                            onClick={() => {
+                                window.location.pathname = val.link
+                            }}
+                        >
+                            <div id="icon">{val.icon}</div> <div id="title" > {val.title} </div>
+                        </li>
+                    );
+                })}
+            </ul>
+            <li
+                key={"logout"}
+                className="rowLogout"
+                onClick={() => { handleClick() }}
+            >
+                <div id="icon">
+                    <LogoutIcon />
+                </div>
+                <div id="title" >
+                    Logout
+                </div>
+            </li>
+        </div>
+    )
 }
 
 export default Sidebar
