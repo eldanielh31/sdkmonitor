776decd06f226421763ed3bb59d348bdc9765b2e
Denis <denisalb@hotmail.com>
2022-07-15 21:13:29 -0600

    MacAddressDatos

--- src/pages/showMac/showMac.jsx
+++ src/pages/showMac/showMac.jsx
@@ -2,17 +2,24 @@ import React from 'react'
 import Sidebar from '../../components/Sidebar'
 import "./showMac.css"
 import { useLocation } from "react-router-dom";
+import MacAddress from '../../components/MacAddress/MacAddress';
 
 function ShowMac() {
 
     const location = useLocation()
-    const macAddress = location.pathname.split("/")[2];
+    const acAddress = location.pathname.split("/")[2];
 
   return (
     <div className='pageSHOWMAC'>
       <Sidebar />
       <div className='containerSHOWMAC'>
-        MacAddress
+        <div>
+          <h1 className='titleMAC'>Mac Address</h1>
+          <div>
+            <h2 className='titleMAC'>Mac ID : 1D:4F:55:89:3A</h2>
+          </div>
+        </div>
+      <MacAddress/>
       </div>
     </div>
   )
