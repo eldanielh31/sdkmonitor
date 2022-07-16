680b3aabd6d32c68dba58f7beef12ed0542fa4b6
Jeffry <jedavid31@gmail.com>
2022-07-16 11:43:30 -0600

    hilo

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@
 import os
 import sys
 from threading import Thread
-from test import main
+from test import  main
+import threading
+
 
 
 class TestThread(Thread):
@@ -14,9 +16,11 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SDKmonitorAPI.settings')
     try:
-        #Inicio thread cpu y memory
-        
         from django.core.management import execute_from_command_line
+        #Inicio thread cpu y memory
+        hilo1= threading.Thread(target=main)
+        hilo1.start()
+        
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
