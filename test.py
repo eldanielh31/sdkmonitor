4058d6e36f3ac3d92c17bc7dbb89d3e691c9816c
Daniel <danbg31@gmail.com>
2022-07-15 22:07:03 -0600

    test saveDict

--- test.py
+++ test.py
@@ -1,53 +1,91 @@
-from cProfile import run
-import psutil
-import json
-from threading import Thread
 from datetime import datetime
 from time import sleep
+import psutil
+import json
 
+diccCPU = {}
+diccMemory = {}
 
-dicCPU={}
-diccMemory={}
+def getCPUPercent():
+    return psutil.cpu_percent()
 
-class SystemUsage:
-    def getCPUPercent():
-        return psutil.cpu_percent()
+def getMemoryPercent():
+    return psutil.virtual_memory().percent
 
-    def getMemoryPercent():
-        return psutil.virtual_memory().percent
+def saveJson():
+    try:
+        with open('cpudata.json', 'w') as f:
+            json.dump(diccCPU, f, indent=4)
+            f.close()
+        with open('memorydata.json', 'w') as f:
+            json.dump(diccMemory, f, indent=4)
+            f.close()
+    except:
+        print('Error al guardar')
+    finally:
+        print('Finalizó el guardado')
 
-def agregarDiccionario(date, time, usage, dictionary):
-    if(date in dictionary):
-        tempObj = dictionary[date]
-        tempObj[time] = usage
-        dictionary[date] = tempObj
+def loadJson():
+    global diccMemory
+    global diccCPU
+    try:
+        with open('cpudata.json', 'r') as f:
+            diccCPU = json.load(f)
+            f.close()
+        with open('memorydata.json', 'r') as f:
+            diccMemory = json.load(f)
+            f.close()
+    except:
+        print("Error al cargar") 
+    finally:
+        print('Finalizó la carga')
+
+def getDate():
+    return datetime.today().strftime("%Y-%m-%d")
+
+def getTime(format):
+    return datetime.now().strftime(format)
+
+def saveInDictCPU():
+    global diccCPU
+
+    dateNow = getDate()
+    timeNowWithSeconds = getTime("%H:%M:%S")
+    timeNowWithoutSeconds = getTime("%H:%M")
+
+    if(dateNow in diccCPU):
+        if (timeNowWithoutSeconds in diccCPU[dateNow]):
+            tempObj = diccCPU[dateNow][timeNowWithoutSeconds]
+            tempObj.append({'time':timeNowWithSeconds, 'value':getCPUPercent()})
+        else:
+            diccCPU[dateNow][timeNowWithoutSeconds] = [
+                {'time': timeNowWithSeconds, 'value': getCPUPercent()}]
     else:
-        dictionary[date] = {time : usage}
+        diccCPU[dateNow] = {timeNowWithoutSeconds: [
+            {'time': timeNowWithSeconds, 'value': getCPUPercent()}]}
 
-class FirstThread(Thread):
-    def run(self):
-        while True:
-        #------------------------------------------------------
-            global dicCPU
-            global diccMemory
-            sleep(5)
-            date=datetime.today().strftime('%Y-%m-%d')
-            currentDateAndTime = datetime.now()
-            time = currentDateAndTime.strftime("%H:%M:%S")
-            agregarDiccionario(date,time,SystemUsage.getCPUPercent(),dicCPU)
-            agregarDiccionario(date,time,SystemUsage.getMemoryPercent(),diccMemory)
-            with open("Memorydata.json","w") as f:
-                json.dump(diccMemory,f,indent=4)   
-                f.close()
-            with open("cpudata.json","w") as f:
-                json.dump(dicCPU,f,indent=4)
-                f.close()
+def saveInDictMemory():
+    global diccMemory
 
-def main():
-    FirstThread().start()
-    while True:
-        print(dicCPU)
-        
-if __name__ == '__main__':
-    main()
+    dateNow = getDate()
+    timeNowWithSeconds = getTime("%H:%M:%S")
+    timeNowWithoutSeconds = getTime("%H:%M")
 
+    if(dateNow in diccMemory):
+        if (timeNowWithoutSeconds in diccMemory[dateNow]):
+            tempObj = diccMemory[dateNow][timeNowWithoutSeconds]
+            tempObj.append({'time': timeNowWithSeconds,
+                           'value': getMemoryPercent()})
+        else:
+            diccMemory[dateNow][timeNowWithoutSeconds] = [
+                {'time': timeNowWithSeconds, 'value': getMemoryPercent()}]
+    else:
+        diccMemory[dateNow] = {timeNowWithoutSeconds: [
+            {'time': timeNowWithSeconds, 'value': getMemoryPercent()}]}
+
+loadJson()
+while True:
+    saveInDictCPU()
+    saveInDictMemory()
+    saveJson()
+    sleep(5)
