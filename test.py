75545ff0244a033b4bb45c1f6cec08defb98db70
Jeffry <jedavid31@gmail.com>
2022-07-15 20:31:42 -0600

    pene

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ from cProfile import run
 import psutil
 import json
 from threading import Thread
-from datetime import date, datetime, timedelta
+from datetime import datetime
 from time import sleep
+
 
 dicCPU={}
 diccMemory={}
@@ -25,30 +26,29 @@ def agregarDiccionario(date, time, usage, dictionary):
 
 class FirstThread(Thread):
     def run(self):
+        while True:
         #------------------------------------------------------
-        global dicCPU
-        global diccMemory
-        sleep(5)
-        date=datetime.today().strftime('%Y-%m-%d')
-        currentDateAndTime = datetime.now()
-        time = currentDateAndTime.strftime("%H:%M:%S")
-        agregarDiccionario(date,time,SystemUsage.getCPUPercent(),dicCPU)
-        agregarDiccionario(date,time,SystemUsage.getMemoryPercent(),diccMemory)
-
+            global dicCPU
+            global diccMemory
+            sleep(5)
+            date=datetime.today().strftime('%Y-%m-%d')
+            currentDateAndTime = datetime.now()
+            time = currentDateAndTime.strftime("%H:%M:%S")
+            agregarDiccionario(date,time,SystemUsage.getCPUPercent(),dicCPU)
+            agregarDiccionario(date,time,SystemUsage.getMemoryPercent(),diccMemory)
+            with open("Memorydata.json","w") as f:
+                json.dump(diccMemory,f,indent=4)   
+                f.close()
+            with open("cpudata.json","w") as f:
+                json.dump(dicCPU,f,indent=4)
+                f.close()
 
 def main():
-    while True:
-        print("...")
-        FirstThread().start()
-        print(dicCPU)
-        print(diccMemory)
-        print(json.dumps(dicCPU, indent=4))
-        with open("cpudata.json","w") as f:
-            json.dump(dicCPU,f,indent=4)
-        print(json.dumps(diccMemory, indent=4))
-        with open("Memorydata.json","w") as f:
-            json.dump(diccMemory,f,indent=4)
-
+        while True:
+            y = json.dumps(dicCPU)
+            print(y)
+            FirstThread().start()
+        
 if __name__ == '__main__':
     main()
 
