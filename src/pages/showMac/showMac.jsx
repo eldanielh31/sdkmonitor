38e364dd0e84e17153c350e49587d02d7e2f76d1
Denis <denisalb@hotmail.com>
2022-07-17 23:46:22 -0600

    MacTable con los datos del data.json

--- src/pages/showMac/showMac.jsx
+++ src/pages/showMac/showMac.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import Sidebar from '../../components/Sidebar'
 import "./showMac.css"
 import { useLocation } from "react-router-dom";
-import MacAddress from '../../components/MacAddress/MacAddress';
 
 function ShowMac() {
 
     const location = useLocation()
-    const acAddress = location.pathname.split("/")[2];
+    const MacAddress = location.pathname.split("/")[2];
 
   return (
     <div className='pageSHOWMAC'>
       <Sidebar />
       <div className='containerSHOWMAC'>
-        {macAddress}
+        {MacAddress}
+        <div>
+          <h1 className='titleMAC'>Mac Address</h1>
+          <div>
+            <h2 className='titleMAC'>Mac ID : 1D:4F:55:89:3A</h2>
+          </div>
+        </div>
+        <MacAddress/>
       </div>
     </div>
   )
