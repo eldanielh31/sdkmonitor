7d81ee8d05f9d4f8955bdb56b0c2d897b1635d39
Daniel <danbg31@gmail.com>
2022-07-20 22:33:13 -0600

    charts terminado

--- src/components/Chart/chart.jsx
+++ src/components/Chart/chart.jsx
@@ -15,14 +15,20 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { useState } from "react";
 import { publicRequest } from "../../requestMethods";
 import { useEffect } from "react";
+import './chart.css'
+
+
 
 function CustomTooltip({ active, payload, label }) {
-    if (active) {
+    if (active && payload && payload.length) {
+        let date = parseISO(label)
         return (
             <div className="tooltip">
-                <h4>{label}</h4>
+                <h4>{format(date, "eeee, d MMM, yyyy")}</h4>
+                <h4>{format(date, "HH:mm:ss")}</h4>
+                <h4>{`Usage: ${payload[0]?.payload?.value}%`}</h4>
                 {/* <h4>{format(parseISO(label), "eeee, d MMM, yyyy")}</h4> */}
-                <p>%{payload[0].value.toFixed(2)}</p>
+                {/* <p>%{payload[0].value.toFixed(2)}</p> */}
             </div>
         );
     }
@@ -31,37 +37,70 @@ function CustomTooltip({ active, payload, label }) {
 
 function Chart(props) {
 
-    const currentDate = new Date();
-    const nextDate = new Date();
-    nextDate.setDate(currentDate.getDate() - 1);
+    const [custom, setCustom] = useState(false);
 
-    const [initialValue, setInitialValue] = useState(currentDate);
-    const [finalValue, setFinalValue] = useState(nextDate);
+    const [initialValue, setInitialValue] = useState();
+    const [finalValue, setFinalValue] = useState();
 
     const [data, setData] = useState([]);
 
-    const getData = async () => {
+    const getData = async (e, initialOrFinal) => {
         try {
-            let date = `${initialValue.getUTCFullYear()}-${("0" + (initialValue.getMonth() + 1)).slice(-2)}-${initialValue.getUTCDate()}`;
-            let date2 = `${finalValue.getUTCFullYear()}-${("0" + (finalValue.getMonth() + 1)).slice(-2)}-${finalValue.getUTCDate()}`;
-            let finaldate = `${date}T${initialValue.getHours()}:${initialValue.getMinutes()}`;
-            let finaldate2 = `${date2}T${finalValue.getHours()}:${finalValue.getMinutes()}`
-            let fulldate = `${finaldate}|${finaldate2}`
+            let inicial = initialOrFinal ? e : initialValue;
+            let final = initialOrFinal? finalValue : e;
+            let date = `${format(inicial, "yyyy-MM-dd")}T${format(inicial, "HH:mm")}`;
+            let date2 = `${format(final, "yyyy-MM-dd")}T${format(final, "HH:mm")}`;
+            let fulldate = `${date}|${date2}`
             const res = await publicRequest.get(`/${props.type}/${fulldate}/`)
             setData(res.data)
-            console.log(res.data)
-
         } catch (error) {
             console.log(error);
         }
     };
 
-    const handleChangeInitial = (newValue) => {
-        setInitialValue(newValue);
-    };
-    const handleChangeFinal = (newValue) => {
-        setFinalValue(newValue);
-    };
+    const handleCustom = async () => {
+        setCustom(true)
+        let currentDate = new Date();
+        let nextDate = new Date();
+        nextDate.setDate(nextDate.getDate() - 1)
+        setInitialValue(nextDate)
+        setFinalValue(currentDate)
+    }
+
+    const handle1Day = async () => {
+        try {
+            setCustom(false)
+            let currentDate = new Date();
+            let nextDate = new Date();
+            nextDate.setDate(nextDate.getDate() - 1)
+
+            let date = `${format(currentDate, "yyyy-MM-dd")}T${format(currentDate, "HH:mm")}`;
+            let date2 = `${format(nextDate, "yyyy-MM-dd")}T${format(nextDate, "HH:mm")}`;
+            let fulldate = `${date2}|${date}`
+            const res = await publicRequest.get(`/${props.type}/${fulldate}/`)
+            setData(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+    const handle1Month = async () => {
+        try {
+            setCustom(false)
+            let currentDate = new Date();
+            let nextDate = new Date();
+            nextDate.setMonth(nextDate.getMonth() - 1)
+
+            let date = `${format(currentDate, "yyyy-MM-dd")}T${format(currentDate, "HH:mm")}`;
+            let date2 = `${format(nextDate, "yyyy-MM-dd")}T${format(nextDate, "HH:mm")}`;
+            let fulldate = `${date2}|${date}`
+            const res = await publicRequest.get(`/${props.type}/${fulldate}/`)
+            setData(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -73,7 +112,45 @@ function Chart(props) {
 
     return (
         <div>
-            <button onClick={getData}>REFRESH</button>
+            <div className="containerButtonsChart">
+                <div className="multi-button">
+                    <button onClick={handle1Day}>1 Day</button>
+                    <button onClick={handle1Month}>1 Month</button>
+                    <button onClick={handleCustom}>Custom Interval</button>
+                </div>
+
+            </div>
+            {
+                custom &&
+                <div className="containerDates" >
+                    <div className="containerDate">
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <DateTimePicker
+                                label="Initial Date&Time"
+                                value={initialValue}
+                                // onChange={handleChangeInitial}
+                                onChange={(e) => { setInitialValue(e) }}
+                                onAccept={(e) => getData(e, true)}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
+                    </div>
+                    <div className="containerDate">
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <DateTimePicker
+                                label="Final Date&Time"
+                                value={finalValue}
+                                // onChange={handleChangeFinal}
+                                onChange={(e) => { setFinalValue(e) }}
+                                onAccept={(e) => getData(e, false)}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
+                    </div>
+                </div>
+
+            }
+
             <ResponsiveContainer width="100%" height={700}>
                 <AreaChart data={data}>
                     <defs>
@@ -90,12 +167,12 @@ function Chart(props) {
                         axisLine={false}
                         tickLine={false}
                         tickFormatter={(str) => {
-                            const date = parseISO(str);
+                            // const date = parseISO(str);
                             // if (date.getDate() % 4 === 0) {
                             //     return format(date, "MMM, d");
                             // }
                             // return "";
-                            const text =  String(str).split(' ')[1]
+                            const text = String(str).split(' ')[1]
                             return text
                         }}
                     />
@@ -107,34 +184,13 @@ function Chart(props) {
                         tickCount={8}
                         tickFormatter={(number) => `${number}%`}
                     />
-                    {/* <Tooltip content={<CustomTooltip />} /> */}
-                    <Tooltip />
+                    <Tooltip content={<CustomTooltip />} />
+                    {/* <Tooltip /> */}
                     <CartesianGrid opacity={0.1} vertical={false} />
                 </AreaChart>
             </ResponsiveContainer>
 
-            <div className="containerDates">
-                <div className="containerDate">
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DateTimePicker
-                            label="Initial Date&Time"
-                            value={initialValue}
-                            onChange={handleChangeInitial}
-                            renderInput={(params) => <TextField {...params} />}
-                        />
-                    </LocalizationProvider>
-                </div>
-                <div className="containerDate">
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DateTimePicker
-                            label="Final Date&Time"
-                            value={finalValue}
-                            onChange={handleChangeFinal}
-                            renderInput={(params) => <TextField {...params} />}
-                        />
-                    </LocalizationProvider>
-                </div>
-            </div>
+
 
         </div>
     )
